4314ac9220f9bb7bc53690fce849b708f566d8ea
rizhalldev <mahmed35@nyit.edu>
2020-05-06 19:02:05 -0400

    Commented ticketsrvtest

--- SSOOS/src/test/java/dev/supremeteam/app/TicketServiceTests.java
+++ SSOOS/src/test/java/dev/supremeteam/app/TicketServiceTests.java
@@ -24,40 +24,40 @@ class TicketServiceTests {
 	@Autowired
 	TicketService ticketServ;
 	
-	@Test
-	@Commit
-	void test() {
-		OrderForm form = new OrderForm();
-		form.setNote("will i have 3 pizzas?");
-		form.setUsername("raymond02");
-		
-		PizzaForm form1 = new PizzaForm();
-		form1.setSize("Small");
-		form1.setType("PlainPizza");
-		
-		PizzaForm form2 = new PizzaForm();
-		form2.setType("VeggiePizza");
-		form2.setSize("Medium");
-		
-		PizzaForm form3 = new PizzaForm();
-		form3.setSize("Large");
-		form3.setType("CustomPizza");
-		Set<String> toppings = new HashSet<String>();
-		toppings.add("Onions");
-		toppings.add("Tomatoes");
-		toppings.add("Ricotta Cheese");
-		toppings.add("Chicken");
-		form3.setToppingNames(toppings);
-		
-		Set<PizzaForm> pizzaForms = new HashSet<PizzaForm>();
-		pizzaForms.add(form1);
-		pizzaForms.add(form2);
-		pizzaForms.add(form3);
-		
-		form.setPizzaForms(pizzaForms);
-		System.out.println(form);
-	//	ticketServ.createTicket(form);
-		//System.out.println();
-	}
+//	@Test
+//	@Commit
+//	void test() {
+//		OrderForm form = new OrderForm();
+//		form.setNote("will i have 3 pizzas?");
+//		form.setUsername("raymond02");
+//		
+//		PizzaForm form1 = new PizzaForm();
+//		form1.setSize("Small");
+//		form1.setType("PlainPizza");
+//		
+//		PizzaForm form2 = new PizzaForm();
+//		form2.setType("VeggiePizza");
+//		form2.setSize("Medium");
+//		
+//		PizzaForm form3 = new PizzaForm();
+//		form3.setSize("Large");
+//		form3.setType("CustomPizza");
+//		Set<String> toppings = new HashSet<String>();
+//		toppings.add("Onions");
+//		toppings.add("Tomatoes");
+//		toppings.add("Ricotta Cheese");
+//		toppings.add("Chicken");
+//		form3.setToppingNames(toppings);
+//		
+//		Set<PizzaForm> pizzaForms = new HashSet<PizzaForm>();
+//		pizzaForms.add(form1);
+//		pizzaForms.add(form2);
+//		pizzaForms.add(form3);
+//		
+//		form.setPizzaForms(pizzaForms);
+//		System.out.println(form);
+//	//	ticketServ.createTicket(form);
+//		//System.out.println();
+//	}
 
 }
