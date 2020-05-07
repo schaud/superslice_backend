31288076e3992e3cec5148f519ede8b821e295eb
rizhalldev <mahmed35@nyit.edu>
2020-05-07 17:09:21 -0400

    here I am

--- SSOOS/src/main/java/dev/supremeteam/app/SsoosApplication.java
+++ SSOOS/src/main/java/dev/supremeteam/app/SsoosApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+// Here I am
 @SpringBootApplication
 @EntityScan("dev.supremeteam.entities")
 @ComponentScan("dev.supremeteam")
